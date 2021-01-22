224bcb0b7af5bf7eb5bb988ebc5b6c7380240230
Oriharara <60921094+Oriharaa@users.noreply.github.com>
2021-01-22 23:40:01 +0900

    50%

---
+++ home.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const semo1 = document.querySelector('.semo1');
+const semo2 = document.querySelector('.semo2');
+const stick1 = document.querySelector('.stick1');
+const stick2 = document.querySelector('.stick2');
+const stick3 = document.querySelector('.stick3');
+const semicircle1 = document.querySelector('.semicircle1');
+const semicircle2 = document.querySelector('.semicircle2');
+const semicircle3 = document.querySelector('.semicircle3');
+const wave1 = document.querySelector('.wave1');
+const wave2 = document.querySelector('.wave2');
+
+document.addEventListener('mousemove', (event)=>{
+    mouseLeftAndRight(event);    
+    mouseUpAndDown(event);
+
+});
+
+function mouseLeftAndRight(event){
+    //마우스가 왼쪽으로 move할 때
+    if(event.clientX <= 739){
+        semo1.style.left = '350px';
+        semo2.style.right = '450px';
+        stick1.style.left = '280px';
+        semicircle1.style.left = '1300px';
+        semicircle2.style.right = '200px';
+        semicircle3.style.left = '50px';
+        
+        //역방향
+        stick2.style.right = '130px';
+        stick3.style.left = '150px';
+        wave1.style.left = '100px';
+        wave2.style.left = '600px';
+
+    //마우스가 오른쪽으로 move할 때    
+    }else if(event.clientX > 739){
+        semo1.style.left = '450px';
+        semo2.style.right = '350px';
+        stick1.style.left = '380px';
+        semicircle1.style.left = '1400px';
+        semicircle2.style.right = '100px';
+        semicircle3.style.left = '150px';
+
+        //역방향
+        stick2.style.right = '230px';
+        stick3.style.left = '50px';
+        wave1.style.left = '0px';
+        wave2.style.left = '500px';
+    }
+};
+
+function mouseUpAndDown(event){
+        //마우스가 아래로 move할 때
+        if(event.clientY > 350){
+            //역방향 
+            semo1.style.top = '100px';
+            semo2.style.top = '100px';
+            stick1.style.top = '-70px';
+            semicircle1.style.top = '-60px';
+            semicircle2.style.bottom = '250px';
+            semicircle3.style.bottom = '150px';
+            
+            //정방향
+            stick2.style.bottom = '-100px';
+            stick3.style.bottom = '-100px';
+            wave1.style.top = '280px';
+            wave2.style.bottom = '-70px';
+            
+        }
+        //마우스가 위로 move할 때
+        else if(event.clientY < 350){
+            //역방향
+            semo1.style.top = '200px';
+            semo2.style.top = '200px';
+            stick1.style.top = '30px';
+            semicircle1.style.top = '40px';
+            semicircle2.style.bottom = '150px';
+            semicircle3.style.bottom = '50px';
+
+            //정방향
+            stick2.style.bottom = '0px';
+            stick3.style.bottom = '0px';
+            wave1.style.top = '180px';
+            wave2.style.bottom = '30px';
+        }
+};
---
+++ main.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const navbar = document.querySelector('#navbar');
+const navbarMenu = document.querySelector('.navbar__menu');
+const home = document.querySelector('#home');
+const homeTitle = document.querySelector('.home__title');
+const homeDescription = document.querySelector('.home__description');
+const homeContact = document.querySelector('.home__contact');
+const arrowIcon = document.querySelector('.fa-long-arrow-alt-right');
+let homeMode = 'normal';
+
+navbarMenu.addEventListener('click', (e)=>{
+    const link = e.target.dataset.link;
+    if(link === '#reverse'){
+        if(homeMode === 'normal'){
+            console.log("리버스모드에 진입합니다.");
+            reverseMode();
+        }else{
+            console.log("노멀모드에 진입합니다.")
+            normalMode();
+            
+        }
+    }
+});
+
+
+function reverseMode(){
+    homeMode = 'reverse';
+    home.style.background = 'url("img/home/background-white.png")center/contain no-repeat';
+    homeTitle.style.color = 'var(--color-dark-navy)';
+    homeDescription.style.color = 'var(--color-dark-navy)';
+    navbar.style.background = 'var(--color-grey-navy)';
+    arrowIcon.style.color = 'darkgreen';
+    homeContact.classList.add('reverse');
+    console.log(`현재 ${homeMode}입니다.`);
+}
+
+function normalMode(){
+    homeMode = 'normal';
+    home.style.background = 'url("img/home/background-dark.png")center/contain no-repeat';
+    home.style.backgroundColor = 'var(--color-dark-black)';
+    homeTitle.style.color = 'var(--color-white)';
+    homeDescription.style.color = 'var(--color-white)';
+    navbar.style.background = 'var(--color-dark-black)';
+    arrowIcon.style.color = 'var(--color-white)';
+    homeContact.classList.remove('reverse');
+    console.log(`현재 ${homeMode}입니다.`);
+}
+
