6c1357b6faf62ff2c119b8c022905eaf5bc24f9b
Oriharara <60921094+Oriharaa@users.noreply.github.com>
2021-01-27 01:24:17 +0900

    f

--- js/project.js
+++ js/project.js
@@ -1,5 +1,30 @@
 'use strict';
 
+const fuck = document.querySelector('#skills');
+const f90 = document.querySelectorAll('.skill__value.graph-90');
+const f70 = document.querySelectorAll('.skill__value.graph-70');
+const f60 = document.querySelectorAll('.skill__value.graph-60');
+const f50 = document.querySelectorAll('.skill__value.graph-50');
+
+
+document.addEventListener('scroll', (e)=>{
+    if(window.scrollY > 1100){
+        f90.forEach(ff90=>{
+            ff90.style.animation = 'graph-90 2s';
+        });
+        f70.forEach(ff70=>{
+            ff70.style.animation = 'graph-70 2s';
+        });
+        f60.forEach(ff60=>{
+            ff60.style.animation = 'graph-60 2s';
+        });
+        f50.forEach(ff50=>{
+            ff50.style.animation = 'graph-50 2s';
+        });
+    }
+});
+
+
 
 
 // const projects = document.querySelectorAll('.project');
