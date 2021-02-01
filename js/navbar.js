f4d019903d03f0b4197ecd4d5dcab2a499f1de80
Oriharara <60921094+Oriharaa@users.noreply.github.com>
2021-02-02 02:44:51 +0900

    JS: active event remove

--- js/navbar.js
+++ js/navbar.js
@@ -45,9 +45,6 @@ homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
   });
 
-// 1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다
-// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
-// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다
 const sectionIds = [
     '#about',
     '#skills',
@@ -58,14 +55,6 @@ const sectionIds = [
   const navItems = sectionIds.map(id =>
     document.querySelector(`[data-link="${id}"]`)
   );
-  
-  let selectedNavIndex = 0;
-  let selectedNavItem = navItems[0];
-  function selectNavItem(selected) {
-    //selectedNavItem.classList.remove('active');
-    selectedNavItem = selected;
-    selectedNavItem.classList.add('active');
-  }
   
   function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
@@ -82,7 +71,6 @@ const sectionIds = [
     entries.forEach(entry => {
       if (!entry.isIntersecting && entry.intersectionRatio > 0) {
         const index = sectionIds.indexOf(`#${entry.target.id}`);
-        // 스크롤링이 아래로 되어서 페이지가 올라옴
       }
     });
   };
