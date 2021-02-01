d2072fdf1d7cfb9af6d3f343a99e53db3d669cc9
Oriharara <60921094+Oriharaa@users.noreply.github.com>
2021-02-01 18:19:28 +0900

    sdafsdaf

---
+++ js/skill.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const skill = document.querySelector('.skill__description h3');
+
+const skillValue = document.querySelectorAll('.skill__value');
+const skillScore = document.querySelectorAll('.skill__bar span');
+const pinger = document.querySelector('.skill__tooltip');
+
+skill.addEventListener('click', ()=>{
+    skillValue.forEach(value=>{
+        value.style.display = 'block';
+        pinger.style.display = 'none';
+        skill.classList.remove('click');
+        
+    });
+});
